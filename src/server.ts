ac370f6033846f4a89ebc022199f4856b44ba214
Victor <paulo9.victor.silva@gmail.com>
2024-12-24 20:54:06 -0300

    feat: add cors

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,16 @@
+import cors from 'cors';
 import express from 'express';
-
 import router from './routes';
 
 const app = express();
 
 app.use(express.json())
+
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 
 app.use(router)
 
